5cda647c973a3790c0dcdfb582ec38c7998a6326
Leonel Viera <vieraleonel1@gmail.com>
2024-04-22 18:44:09 -0300

    CORS y get por ID de especie

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import { count, sql } from "drizzle-orm";
@@ -9,7 +10,7 @@ import { readFile } from "fs/promises";
 
 // setup DB
 const sqlite = new Database("sqlite.db");
-const db = drizzle(sqlite);
+const db = drizzle(sqlite, { schema: { ...especies } });
 
 migrate(db, { migrationsFolder: "migrations-folder" });
 db.select({ count: count() })
@@ -26,6 +27,8 @@ db.select({ count: count() })
   });
 
 const app = new Hono();
+app.use("*", cors({ origin: "*" }));
+app.notFound((c) => c.json({ message: "Not found" }, { status: 404 }));
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
@@ -34,6 +37,17 @@ app.get("/", (c) => {
 app.get("/especies", async (c) => {
   const result = await db.select().from(especies).execute();
   return c.json(result);
+});
+
+app.get("/especies/:id", async (c) => {
+  const result = await db
+    .select()
+    .from(especies)
+    .where(sql`sp_id = ${c.req.param("id")}`)
+    .limit(1)
+    .execute();
+  const especie = result?.[0] ?? null;
+  return c.json(especie);
 });
 
 const port = 3000;
